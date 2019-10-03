cc92ab92d77c9c0aa667a66ebbad709778db5739
Robert Lin <rcl2106@gmail.com>
2019-10-03 11:47:55 -0400

    Add Python Django web frontend. This will later be deployed to Heroku to expose Python analysis tools for Kaggle challenges.

---
+++ hello/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+from .models import Greeting
+
+# Create your views here.
+def index(request):
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('Welcome to CincyAI!<br /><br /><pre>' + r.text + '</pre>')
+# def index(request):
+#     # return HttpResponse('Hello from Python!')
+#     return render(request, "index.html")
+
+
+def db(request):
+
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, "db.html", {"greetings": greetings})
